3a9d77d7cde6d7b903681228d7c0a35c6758acb0
mjamontano <147313942+mjamontano@users.noreply.github.com>
2025-03-02 15:19:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Get the button
+const scrollUpBtn = document.getElementById('scrollUpBtn');
+
+// Show the button when the user scrolls down
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollUpBtn.style.display = 'block';  // Show the button
+    } else {
+        scrollUpBtn.style.display = 'none';  // Hide the button
+    }
+};
+
+// Scroll to the top of the page when the button is clicked
+scrollUpBtn.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
+
 // Function to open modal
 function openModal(modalId) {
   var modal = document.getElementById(modalId);
@@ -22,3 +42,5 @@ closeButtons.forEach(function(closeButton) {
     modal.style.display = "none";
   });
 });
+
+
